3a95f5e984201967096401310ca1626535a999b5
akashkewat99 <akashkewat99@gmail.com>
2025-02-09 00:30:19 +0530

    add

--- 02Param_Query_FormsSubmision/src/index.js
+++ 02Param_Query_FormsSubmision/src/index.js
@@ -5,26 +5,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const staticFilePath = path.join(__dirname, "..", "public");
-app.use(express.static(staticFilePath));
-
-app.get("/", (req, res) => {
-  const filePath = path.join(__dirname, "..", "public", "index.html");
-  res.sendFile(filePath);
-});
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.post("/contact", (req, res) => {
   console.log(req.body);
   res.redirect("/");
 });
 
-
 // ! error page handle  // must be at end
-app.use((req, res)=>{
+app.use((req, res) => {
   // ? return res.status(404).send("Page not found")
-  const filePath = path.join(__dirname, "..", "views", "404.html")
-  return res.status(404).sendFile(filePath)
-})
+  const filePath = path.join(__dirname, "..", "views", "404.html");
+  return res.status(404).sendFile(filePath);
+});
 
 app.listen(3000, () => {
   console.log(`⚙️ Server is listening at 3000`);
